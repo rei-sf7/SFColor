8a24c438c77091bae32e6f59e649f0e5c5be0989
s.rei <>
2024-03-10 15:27:30 +0900

    カラーパターン追加

--- SFColor.swift
+++ SFColor.swift
@@ -4,9 +4,18 @@ import SpriteKit
 final class SFColor {
     enum ColorName: String {
         case blueWithHintsOfBlue = "BlueWithHintsOfBlue"
+        case chartreuseYellow = "ChartreuseYellow"
         case dark = "Dark"
+        case fireRed = "FireRed"
+        case gold = "Gold"
+        case lilyMagnolia = "LilyMagnolia"
+        case madderRed = "MadderRed"
+        case matcha = "Matcha"
         case murkyBlue = "MurkyBlue"
         case mutedBluishPurple = "MutedBluish-Purple"
+        case rose = "Rose"
+        case rubyRed = "RubyRed"
+        case scarlet = "Scarlet"
         case white = "White"
     }
     func getColor(_ color: SFColor.ColorName) -> UIColor {
